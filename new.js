dc42f0544ef4232f66ef3d7566de5fcf2a14a00e
Sneha <sneha.adlakha@gmail.com>
2020-12-19 15:41:10 +0530

    final

--- new.js
+++ new.js
@@ -1,11 +1,34 @@
 var buttonTranslate=document.querySelector('#buttonTranslate');
 var textOne=document.querySelector('#txtOne');
 var outputdiv=document.querySelector("#output");
+serverUrl="https://api.funtranslations.com/translate/minion.json"
 
-buttonTranslate.addEventListener('click',ClickEventHandler);
+function constructUrl(text){
+   return serverUrl+"?"+""+"text="+text;
+}
+
+function errorHandler(error){
+   console.log("error occured",error);
+   alert("server not responding");
+}
 
 function ClickEventHandler(){
-console.log("clicked");
-   outputdiv.innerText=textOne.value;
+   var inputText=textOne.value;
+   fetch(constructUrl(inputText))
+      .then(response=>response.json())
+      .then(json=>
+         {
+            var translatedValue=json.contents.translated;
+            outputdiv.innerText=translatedValue;
+         })
+         .catch(errorHandler)
+};
+function clearHandler(){
+   textOne.value="";
+   outputdiv.innerText="";
 
 }
+buttonTranslate.addEventListener('click',ClickEventHandler);
+var buttonClear=document.querySelector('#button-Clear');
+buttonClear.addEventListener('click',clearHandler);
+
